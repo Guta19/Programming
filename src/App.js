a2fbf947751ee493f02a164e6b7c062f89634ce2
Guta19 <guta.neme@aau.edu.et>
2024-03-25 23:33:18 +0300

    Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import FunctionalComp from './Components/FunctionalComp';
+import ClassComp from './Components/ClassComp';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>Hello! Welcome to React!</h1>
+      <h1>This is the first time here</h1>
+      <FunctionalComp></FunctionalComp>
+      <ClassComp></ClassComp>
     </div>
-  );
+  )
+ 
+  
 }
 
 export default App;
